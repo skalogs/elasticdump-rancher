929c5d54fbeb3081ae0c461d26eb32073a943fae
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2017-12-14 19:17:44 +0100

    better check elasticsearch and check kibana

--- startup.sh
+++ startup.sh
@@ -9,24 +9,37 @@ else
   ES_AUTH="${SERVICE_ELASTICSEARCH_USERNAME}:${SERVICE_ELASTICSEARCH_PASSWORD}@"
 fi
 ES_URL=http://${ES_AUTH}${SERVICE_ELASTICSEARCH_HOST}:${SERVICE_ELASTICSEARCH_PORT}
+KIBANA_URL=http://${ES_AUTH}${SERVICE_KIBANA_HOST}:${SERVICE_KIBANA_PORT}
 
 function checkElasticsearch {
-    a="`curl ${ES_URL}/_cluster/health &> /dev/null; echo $?`"
-    while  [ $a -ne 0 ];
+    curlCommand="curl --silent ${ES_URL}/_cluster/health -H 'Content-Type: application/json;charset=UTF-8' --write-out %{http_code} --output /dev/null"
+    status=$(eval $curlCommand)
+    while  [ $status -ne 200 ] ;
     do
-        a="`curl ${ES_URL}/_cluster/health &> /dev/null; echo $?`"
+        status=$(eval $curlCommand)
+        sleep 1
+    done
+}
+
+function checkKibana {
+    curlCommand="curl --silent ${KIBANA_URL}/api/status -H 'Content-Type: application/json;charset=UTF-8' --write-out %{http_code} --output /dev/null"
+    status=$(eval $curlCommand)
+    while  [ $status -ne 200 ] ;
+    do
+        status=$(eval $curlCommand)
         sleep 1
     done
 }
 
 checkElasticsearch
+checkKibana
 
 echo "Restoring elasticsearch dump"
 curl ${RANCHER_BASEURL}/self/service/metadata/elasticdump > /tmp/elasticdump.json
 /usr/lib/node_modules/elasticdump/bin/elasticdump --input=/tmp/elasticdump.json --output=${ES_URL}/${TARGET_INDEX} --headers='{"Content-Type": "application/json"}' ${ELASTICDUMP_OPTS}
 
 if [ ! -z "${DEFAULT_INDEX_PATTERN}" ]; then
-    curl http://${ES_AUTH}${SERVICE_KIBANA_HOST}:${SERVICE_KIBANA_PORT}/api/kibana/settings/defaultIndex \
+    curl ${KIBANA_URL}/api/kibana/settings/defaultIndex \
 	    -H "Content-Type: application/json" \
 	    -H "kbn-xsrf: anything" \
 	    --data-binary '{"value":"'${DEFAULT_INDEX_PATTERN}'"}' \
